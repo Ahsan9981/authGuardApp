bdff463346ab42a97294f98967936d0f026f2a7c
Mohammad Ahsan <m.ahsan9981@gmail.com>
2018-12-10 15:54:57 +0500

    guard service implemented and authentication service consumption.

--- src/app/guards/auth-guard.service.ts
+++ src/app/guards/auth-guard.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import { Route, Router, CanActivate, 
+  ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable } from 'rxjs';
+
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private _authService: AuthService,
+              private router: Router) { }
+
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+   Observable<boolean> | Promise<boolean> | boolean {
+    
+    if(this._authService.isAuthenticated()) {
+      return true;
+    }
+    // navigate to login page
+    this.router.navigate(['/login']);
+    // you can save redirect url so after authing we can move them back to the page they requested
+    return false;
+  }
 }
