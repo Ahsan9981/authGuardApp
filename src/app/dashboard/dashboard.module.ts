7986f12f0d1bed634c95c2db3ce07d6b3e612752
Mohammad Ahsan <m.ahsan9981@gmail.com>
2018-12-10 13:58:16 +0500

    routing for dashboard components added.

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout/layout.component';
 import { HomeComponent } from './home/home.component';
 import { AdminComponent } from './admin/admin.component';
+import { DashboardRoutingModule } from './dashboard-routing.module';
 
 @NgModule({
   declarations: [LayoutComponent, HomeComponent, AdminComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    DashboardRoutingModule
   ]
 })
 export class DashboardModule { }
